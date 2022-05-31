c746120e2010506455f141369e4ef923ff3f9384
Wasi Al Rahman <51267405+JohnConstantine94@users.noreply.github.com>
2022-05-31 16:32:21 +0600

    Add files via upload

---
+++ Frontend Task 1/function.js
@@ -0,0 +1,23 @@
+function popunder(){
+    document.getElementById('image').src="popunder.jpg";
+    document.getElementById('message').innerHTML="POP-UNDER";
+    document.getElementById('message2').innerHTML="This is one of the most popular ads. After user’s click, this ad opens landing page in behind. So, the user experience is not hampered much.";
+}
+
+function bannerad(){
+    document.getElementById('image').src="bannerad.png";
+    document.getElementById('message').innerHTML="BANNER AD";
+    document.getElementById('message2').innerHTML="Choose between the available banner formats. Highly effective ad for visibility.";
+}
+
+function native(){
+    document.getElementById('image').src="nativead.jpg";
+    document.getElementById('message').innerHTML="NATIVE";
+    document.getElementById('message2').innerHTML="Ad format with the image and the title, that perfectly fits into the content of the site. Native ads are placed in the most visible areas of the page and, thus, this format shows good results and the level of interaction.";
+}
+
+function skim(){
+    document.getElementById('image').src="skim.jpg";
+    document.getElementById('message').innerHTML="SKIM";
+    document.getElementById('message2').innerHTML="SKIM is a link that a publisher inserts on a specific site element, and after clicking on it, an advertisement appears to the user. SKIM gives 100% control of your sold traffic amount, higher CR to advertisers and CPM rates to publishers.";
+}
